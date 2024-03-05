04f17e82d1a0037c07962e7d8d390c11bc94516e
Ddg80 <dguillin80@gmail.com>
2024-03-05 12:02:24 +0100

    Chore(feature): add route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 // Server Setup
 const PORT = 3333;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
 app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
