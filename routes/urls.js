3a0b9827b4dde0bb614d7ed55cbc59a31483cded
Ddg80 <dguillin80@gmail.com>
2024-03-30 15:54:38 +0100

    feat(urls.js): Add router and export

--- routes/urls.js
+++ routes/urls.js
@@ -1,5 +1,37 @@
 import express from "express";
 import Url from "../models/Url";
-import dotenv from 'dotenv'
-dotenv.config({path: '.env'})
+import dotenv from "dotenv";
+import { nanoid } from "nanoid";
+dotenv.config({ path: ".env" });
 
+const router = express.Router();
+
+router.post("/post", async (req, res) => {
+  const { origUrl } = req.body;
+  const base = process.env.BASE;
+
+  const urlId = nanoid();
+  if (utils.validateUrl(origUrl)) {
+    try {
+      let url = await Url.findOne({ origUrl });
+      if (url) {
+        res.json(url);
+      } else {
+        const shortUrl = `${base}/${urlId}`;
+        url = new Url({
+          origUrl,
+          shortUrl,
+          urlId,
+          date: new Date(),
+        });
+        await url.save();
+        res.json(url);
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(500).json("Server Error");
+    }
+  }else {
+    res.status(400).json('Invalid Original Url')
+  }
+});
